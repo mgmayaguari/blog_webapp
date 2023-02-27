4435c3a20538493d957bd6ad4a8a15a23e4a9cc8
Michael Mayaguari <mayaguarimichael@gmail.com>
2023-02-27 18:05:21 -0500

    sign up page form added to frontend

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   has_many :articles
+  before_save {self.email = email.downcase }
 
   validates :username, presence: true, length: { minimum: 3, maximum: 25 }, 
     uniqueness: { case_sensitivity: false}
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitivity: false},
     format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
+
 end
