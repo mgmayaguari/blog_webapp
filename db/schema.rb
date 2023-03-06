a7be4ad0025eaf223dc8b0b2c6b0641fcb0d1737
Michael Mayaguari <mayaguarimichael@gmail.com>
2023-03-06 11:00:47 -0500

    many-to-many article and category association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_165112) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_154906) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
